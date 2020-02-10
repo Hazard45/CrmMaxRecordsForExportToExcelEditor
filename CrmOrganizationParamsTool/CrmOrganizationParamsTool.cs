629645de182772ba659a7a4aa3000251d66f55f4
Arsen Tadevosyan <arsen20391@gmail.com>
2020-02-10 11:51:15 +0400

    Upgraded to organization parameters editor tool

---
+++ CrmOrganizationParamsTool/CrmOrganizationParamsTool.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Description;
+using System.Windows.Forms;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CrmOrganizationParamsTool
+{
+    public partial class CrmOrganizationParamsTool : Form
+    {
+        private IOrganizationService service;
+        private Entity organizationEntity;
+
+        public CrmOrganizationParamsTool()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var uri = new Uri(TextBoxOrganizationServiceUrl.Text);
+                var credentials = new ClientCredentials();
+                if (!string.IsNullOrWhiteSpace(TextBoxUserName.Text))
+                {
+                    credentials.UserName.UserName = TextBoxUserName.Text;
+                    credentials.UserName.Password = TextBoxPassword.Text;
+                }
+                else
+                {
+                    credentials.Windows.ClientCredential = CredentialCache.DefaultNetworkCredentials;
+                }
+
+                service = new OrganizationServiceProxy(uri, null, credentials, null);
+
+                GroupBoxOrganizationParameters.Enabled = true;
+                GetData(0);
+                ComboBoxParameter.DroppedDown = true;
+            }
+            catch (Exception ex)
+            {
+                ComboBoxParameter.Items.Clear();
+                TextBoxParameterType.Text = string.Empty;
+                TextBoxParameterValue.Text = string.Empty;
+                GroupBoxOrganizationParameters.Enabled = false;
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        private void TextBoxOrganizationServiceUrl_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TextBoxOrganizationServiceUrl.Text))
+            {
+                ButtonConnect.Enabled = false;
+            }
+            else
+            {
+                ButtonConnect.Enabled = true;
+            }
+        }
+
+        private void ComboBoxParameterName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ComboBoxParameter.SelectedIndex >= 0)
+            {
+                try
+                {
+                    var param = (OrganizationParam)ComboBoxParameter.SelectedItem;
+                    TextBoxParameterType.Text = param.GetValueTypeString();
+                    TextBoxParameterValue.Text = param.GetValueString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error");
+                }
+            }
+        }
+
+        private void ButtonSetValue_Click(object sender, EventArgs e)
+        {
+            if (ComboBoxParameter.SelectedIndex >= 0)
+            {
+                try
+                {
+                    var param = (OrganizationParam)ComboBoxParameter.SelectedItem;
+                    param.SetValue(TextBoxParameterValue.Text.Trim());
+                    organizationEntity[param.Name] = param.Value;
+                    service.Update(organizationEntity);
+                    GetData(ComboBoxParameter.SelectedIndex);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Error");
+                }
+            }
+        }
+
+        private void GetData(int selectedIndex)
+        {
+            ComboBoxParameter.Items.Clear();
+            var query = new QueryExpression("organization");
+            query.ColumnSet = new ColumnSet(true);
+            var organizations = service.RetrieveMultiple(query);
+            if (organizations.Entities.Count == 1)
+            {
+                organizationEntity = organizations.Entities[0];
+                var parameters = organizationEntity.Attributes.ToList();
+                foreach (var param in parameters.OrderBy(p => p.Key))
+                {
+                    var orgParam = new OrganizationParam(param.Key, param.Value);
+                    ComboBoxParameter.Items.Add(orgParam);
+                    if (ComboBoxParameter.Items.Count > selectedIndex)
+                    {
+                        ComboBoxParameter.SelectedIndex = selectedIndex;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("There is no such organization");
+            }
+        }
+    }
+}
---
+++ CrmOrganizationParamsTool/OrganizationParam.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace CrmOrganizationParamsTool
+{
+    public class OrganizationParam
+    {
+        public string Name { get; set; }
+
+        public object Value { get; set; }
+
+        public OrganizationParam(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string GetValueString()
+        {
+            if (Value is EntityReference)
+            {
+                var value = (EntityReference)Value;
+                return value.LogicalName + " " + value.Id;
+            }
+            else if (Value is OptionSetValue)
+            {
+                var value = (OptionSetValue)Value;
+                return value.Value.ToString();
+            }
+            else
+            {
+                return Value.ToString();
+            }
+        }
+
+        public string GetValueTypeString()
+        {
+            return Value.GetType().ToString().Replace("Microsoft.Xrm.Sdk.", string.Empty).Replace("System.", string.Empty);
+        }
+
+        public void SetValue(string valueString)
+        {
+            if (Value is EntityReference)
+            {
+                if (valueString.Length >= 36)
+                {
+                    var idString = valueString.Substring(valueString.Length - 36);
+                    var textString = valueString.Replace(idString, string.Empty).Trim();
+                    Guid id;
+                    if (Guid.TryParse(idString, out id))
+                    {
+                        Value = new EntityReference(textString, id);
+                    }
+                    else
+                    {
+                        throw new Exception("Wrong value type format for EntityReference." + Environment.NewLine + "Value format must be: entity_name entity_guid" + Environment.NewLine);
+                    }
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            else if (Value is OptionSetValue)
+            {
+                int number;
+                if (int.TryParse(valueString, out number))
+                {
+                    Value = new OptionSetValue(number);
+                }
+                else
+                {
+                    throw new Exception("Wrong value type format for OptionSetValue." + Environment.NewLine + "Value must be an integer number" + Environment.NewLine);
+                }
+            }
+            else if (Value is int)
+            {
+                int number;
+                if (int.TryParse(valueString, out number))
+                {
+                    Value = number;
+                }
+                else
+                {
+                    throw new Exception("Wrong value type format for Int." + Environment.NewLine + "Value must be an integer number" + Environment.NewLine);
+                }
+            }
+            else if (Value is bool)
+            {
+                bool boolean;
+                if (bool.TryParse(valueString, out boolean))
+                {
+                    Value = boolean;
+                }
+                else
+                {
+                    throw new Exception("Wrong value type format for Bool." + Environment.NewLine + "Value must be True or False" + Environment.NewLine);
+                }
+            }
+            else
+            {
+                Value = valueString;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + " = " + Value.ToString();
+        }
+    }
+}
